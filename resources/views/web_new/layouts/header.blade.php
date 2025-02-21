2d3e7196cf3efea3056bc50de6dbeea968713c16
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-02-21 18:31:34 +0530

    add header csrf token

--- resources/views/web_new/layouts/header.blade.php
+++ resources/views/web_new/layouts/header.blade.php
@@ -5,6 +5,7 @@
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="robots" content="noindex, nofollow" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
       integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
