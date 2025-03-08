a1fc91f9e93b3b1d628da57d1d1d9a3893bad50a
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-03-08 15:25:04 +0530

    doctor plan add exercise isse

--- resources/views/admin/doctor/plan/add-new.blade.php
+++ resources/views/admin/doctor/plan/add-new.blade.php
@@ -374,8 +374,9 @@ $.ajaxSetup({
 	$('body').on('click', '.addrow, .addadminrow', function () {
 		console.log('call time');
     	var itemrow = $('#itemrow').clone(); // Remove the duplicate id from the cloned row
-		var exercise_id = $('select[id^="exercise_id"]:last');
-		var num = parseInt(exercise_id.prop("id").match(/\d+/g), 10) + 1;
+		// var exercise_id = $('select[id^="exercise_id"]:last');
+		var category_id = $('select[id^="category_id"]:last');
+		var num = parseInt(category_id.prop("id").match(/\d+/g), 10) + 1;
 		var is_admin = $(this).hasClass('addadminrow') ? 1 : 0;
 		itemrow.find("select, input").each(function () {
 			var currentId = $(this).attr('id');
