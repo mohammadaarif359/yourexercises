d4aaf26b5e7b013f92b4cf91d38032f99cfe4f1e
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-08 11:56:45 +0530

    plan assign feedback common ode email

--- app/Traits/AuthCode.php
+++ app/Traits/AuthCode.php
@@ -10,7 +10,7 @@ use App\Mail\OtpEmail;
 use App\Mail\PasswordReset;
 use App\Mail\PatientUserCreate;
 use App\Mail\Doctor\ProfileVerified;
-use App\Mail\Patient\{PlanAssign, PlanAssignStatus};
+use App\Mail\Patient\{PlanAssign, PlanAssignStatus, PlanAssignFeedback};
 use Mail;
 use Exception;
 use Twilio\Rest\Client;
@@ -139,6 +139,10 @@ trait AuthCode
 	}
 	public function sendPatientPlanAssignStatusEmail($data){
 		Mail::to($data['email'])->send(new PlanAssignStatus($data));
+		return true;
+	}
+	public function sendPatientPlanAssignFeedbackEmail($data){
+		Mail::to($data['email'])->send(new PlanAssignFeedback($data));
 		return true;
 	}
 	public function uploadImg($file, $path,$path_type='public')
