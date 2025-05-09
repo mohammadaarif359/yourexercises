27b19d1a1dab469de50d1c80634dd5c8c11f527a
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-09 13:14:14 +0530

    fix lyout ussue

--- resources/views/web_new/about.blade.php
+++ resources/views/web_new/about.blade.php
@@ -78,8 +78,8 @@
         <div class="core-values-section">
             <div class="sidebar">
                 <h1>What We Offer</h1>
-                <p>Yourexercises.com is designed to transform the way physiotherapists and patients interact. With a focus on ease of use, automation, and personalization, we provide a suite of features that ensure a smoother experience for both patients and their healthcare providers</p>
             </div>
+            <p>Yourexercises.com is designed to transform the way physiotherapists and patients interact. With a focus on ease of use, automation, and personalization, we provide a suite of features that ensure a smoother experience for both patients and their healthcare providers</p>
             <div class="values-content">
                 <div class="value-item">
                     <div class="d-flex gap-5">
