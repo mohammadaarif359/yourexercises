cd5ee10538d83a8b1c90d9c7eb4935366033b5b2
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-09 13:08:47 +0530

    about ceo

--- resources/views/web_new/about.blade.php
+++ resources/views/web_new/about.blade.php
@@ -132,7 +132,7 @@
                 <p>
                     We are passionate about <b>making a difference</b>, and our goal is simple: to offer a modern platform that delivers the tools needed to improve health outcomes in an accessible, efficient, and user-friendly way
                 </p>
-                
+
                 <h2>
                     Why Choose Us?
                 </h2>
@@ -149,6 +149,12 @@
                     <strong>Tailored to You:</strong> Whether you are a physiotherapist looking for a better way to manage your practice or a patient seeking a personalized rehabilitation plan, Yourexercises.com adapts to your needs
                 </p>
             </div>
+
+            <h2>Join Us in Shaping the Future of Physiotherapy</h2>
+            <p>Thank you for choosing Yourexercises.com. Together, we can make rehabilitation smarter, easier, and more impactful. Whether you're a physiotherapist or a patient, we are here to help you every step of the way as we work toward a healthier, technology-driven future.</p>
+            <p><strong>- Jeet & Drashti</strong><br/>
+            Co-founders of Yourexercises.com<br/>
+            Your trusted partners in health and technology innovation</p>
         </div>   
 
         <!--<div class="container ps-eml" data-aos="fade-up" data-aos-duration="1000">
