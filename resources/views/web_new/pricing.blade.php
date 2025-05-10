3c47d3147daea7fc8693fe5812675a4b972d978a
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-10 10:07:25 +0530

    old pricing with query params

--- resources/views/web_new/pricing.blade.php
+++ resources/views/web_new/pricing.blade.php
@@ -13,6 +13,174 @@
             </div>
         </div>
 
+        @if($old_pricing)
+        <div class="ps-pricing-set">
+            <div class="container ">
+                <div class="row py-5">
+                    <div class="col-md-4 mb-4" data-aos="fade-left" data-aos-duration="1000">
+                        <div class="card ps-card">
+                            <div class="card-header paragraph-lg cl-dblue">
+                                Balance
+                            </div>
+                            <div class="card-body">
+                                <div class="price">
+                                    <p>CAD</p> <span>$54 </span>/ month
+                                </div>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Single practitioner profile</li>
+                                    <li><i class="fas fa-check"></i> Up to 20 appointments per month</li>
+                                    <li><i class="fas fa-check"></i> Unlimited admin & locations</li>
+                                </ul>
+                                <button class="ps-btn sm-btn primary-btn">Sign up</button>
+                                <button class="ps-btn sm-btn outline-btn">Book a demo</button>
+                            </div>
+                            <div class="card-body mt-3">
+                                <h6>Scheduling:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Online 1:1 Appointments (Telehealth)</li>
+                                    <li><i class="fas fa-check"></i> In-Person 1:1 Appointments, Group Appointments, and
+                                        Classes
+                                    </li>
+                                </ul>
+                                <h6>Patient Communication:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Unlimited Email Appointment Reminders</li>
+                                    <li><i class="fas fa-check"></i> Secure Patient Portal</li>
+                                </ul>
+                                <h6>Customizable Documentation & Clinical Tracking:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Charting Templates</li>
+                                    <li><i class="fas fa-check"></i> Intake Forms</li>
+                                    <li><i class="fas fa-check"></i> Outcome Measure Surveys</li>
+                                    <li><i class="fas fa-check"></i> Treatment Plans</li>
+                                    <li><i class="fas fa-check"></i> Supervision</li>
+                                    <li><i class="fas fa-check"></i> Integrated Outbound Fax<sup>*</sup></li>
+                                </ul>
+                                <h6>Unlimited Human Support:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Free Data Import</li>
+                                    <li><i class="fas fa-check"></i> Chat, Email, and Phone Support</li>
+                                </ul>
+                            </div>
+                            <div class="card-footer pt-3 p-0 bg-light">
+                                <p>Group telehealth made for Your Exercises clinics: ($15 / month per practitioner)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-4" data-aos="zoom-in" data-aos-duration="1000">
+                        <div class="card ps-card">
+                            <div class="card-header paragraph-lg cl-dblue">
+                                Practice
+                            </div>
+                            <div class="card-body">
+                                <div class="price">
+                                    <p>CAD</p> <span>$79 </span>/ month
+                                </div>
+                                <p class="cl-gray paragraph-md mb-0">Plus $17.50 / month per half license</p>
+                                <p class="cl-gray paragraph-md mb-0">Plus $35 / month per full license</p>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Unlimited admin & locations</li>
+                                </ul>
+                                <button class="ps-btn sm-btn primary-btn mb-2">Sign up</button>
+                                <button class="ps-btn sm-btn outline-btn">Book a demo</button>
+                            </div>
+                            <div class="card-body">
+                                <h6>Everything from Balance, plus:</h6>
+                                <h6>Unlimited Usage:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Unlimited Appointments</li>
+                                    <li><i class="fas fa-check"></i> Unlimited Staff Profiles</li>
+                                </ul>
+                                <h6>Online Booking:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Customizable Branding</li>
+                                    <li><i class="fas fa-check"></i> Selectable Availability</li>
+                                    <li><i class="fas fa-check"></i> Online Booking Pre-Payment Policies</li>
+                                    <li><i class="fas fa-check"></i> Reserve with Google</li>
+                                </ul>
+                                <h6>Patient Communication:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Unlimited, free SMS Reminders & Notifications</li>
+                                </ul>
+                            </div>
+                            <div class="card-footer pt-3 p-0 bg-light">
+                                <div class="form-check-div">
+                                    <input class="form-check-input" type="checkbox" id="insuranceBillingPractice">
+                                    <label class="form-check-label" for="insuranceBillingPractice">
+                                        Add Insurance Billing
+                                    </label>
+                                </div>
+                                <ul class="list-unstyled mt-3 mb-3">
+                                    <li><i class="fas fa-times"></i> Insurance Invoicing & Payment Tracking</li>
+                                    <li><i class="fas fa-times"></i> Integrated Direct Insurance Billing for a growing
+                                        list: TELUS
+                                        eClaims, Teleplan, and Pacific Blue Cross PROVIDERnet.ca</li>
+                                    <li><i class="fas fa-times"></i> Insurance Card Photo Upload in Intake Forms</li>
+                                </ul>
+                                <p class="paragraph-lg mb-0">Group telehealth made for Your Exercises clinics: ($15 / month per
+                                    practitioner)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-4" data-aos="fade-right" data-aos-duration="1000">
+                        <div class="card ps-card">
+                            <div class="card-header paragraph-lg cl-dblue">
+                                Thrive
+                            </div>
+                            <div class="card-body">
+                                <div class="price">
+                                    <p>CAD</p> <span>$99 </span>/ month
+                                </div>
+                                <p class="cl-gray paragraph-md mb-0">Plus $20 / month per half license</p>
+                                <p class="cl-gray paragraph-md mb-0">Plus $40 / month per full license</p>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Unlimited admin & locations</li>
+                                </ul>
+                                <button class="ps-btn sm-btn primary-btn mb-2">Sign up</button>
+                                <button class="ps-btn sm-btn outline-btn">Book a demo</button>
+                            </div>
+                            <div class="card-body">
+                                <h6>Everything from Practice, plus:</h6>
+                                <h6>Advanced Scheduling:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Rooms & Equipment</li>
+                                    <li><i class="fas fa-check"></i> Availability Tagging</li>
+                                    <li><i class="fas fa-check"></i> Patient Self Check-in (In-Person)</li>
+                                    <li><i class="fas fa-check"></i> Automated Patient Waitlist Management</li>
+                                </ul>
+                                <h6>Patient Retention & Engagement:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Return Visit Reminders</li>
+                                    <li><i class="fas fa-check"></i> Packages & Memberships</li>
+                                </ul>
+                                <h6>Marketing Tools:</h6>
+                                <ul class="list-unstyled">
+                                    <li><i class="fas fa-check"></i> Ratings and Reviews</li>
+                                </ul>
+                            </div>
+                            <div class="card-footer pt-3 p-0 bg-light">
+                                <div class="form-check-div">
+                                    <input class="form-check-input" type="checkbox" id="insuranceBillingThrive">
+                                    <label class="form-check-label" for="insuranceBillingThrive">
+                                        Add Insurance Billing
+                                    </label>
+                                </div>
+                                <ul class="list-unstyled mt-3 mb-3">
+                                    <li><i class="fas fa-times"></i> Insurance Invoicing & Payment Tracking</li>
+                                    <li><i class="fas fa-times"></i> Integrated Direct Insurance Billing for a growing
+                                        list: TELUS
+                                        eClaims, Teleplan, and Pacific Blue Cross PROVIDERnet.ca</li>
+                                    <li><i class="fas fa-times"></i> Insurance Card Photo Upload in Intake Forms</li>
+                                </ul>
+                                <p class="paragraph-lg mb-0">Group telehealth made for Your Exercises clinics: ($15 / month per
+                                    practitioner)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
 
         <div class="container">
 
