dc211d1b09b5a243c440f390b471cadaedf06df6
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-17 12:22:41 +0530

    cmd cosole name give

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('notify:last-login-patient')->everyMinute();
+        $schedule->command('notify:patient-plan-not-regular')->everyMinute();
 
     }
 
