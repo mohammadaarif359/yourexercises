5aab65cd677663fe432218144aead536c1d4c810
mohammadaarif359 <mohammedaarif359@gmail.com>
2025-05-24 12:21:44 +0530

    logout doctor without profile redirect issue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -116,7 +116,7 @@ class AuthController extends Controller
         Session::flush();
 		Auth::guard('web')->logout();
         $redirectTo = '/login';
-        if($user->hasRole('doctor')) {
+        if($user->hasRole('doctor') && $user->doctor_profile) {
             $redirectTo = '/clinic/'.$user->doctor_profile->slug;
         } else if($user->hasRole('patient')) {
             $redirectTo = $user->patient_doctor_profile ? 'clinic/'.$user->patient_doctor_profile['slug'] :  '/login';
