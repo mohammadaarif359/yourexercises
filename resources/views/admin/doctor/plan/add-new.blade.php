d60d2f6bd83b7f21c199d188b8d3f5636471699d
aarif <98820502+mohammadaarif359@users.noreply.github.com>
2025-11-15 12:14:50 +0530

    Update add-new.blade.php
    exercise poup img szie change

--- resources/views/admin/doctor/plan/add-new.blade.php
+++ resources/views/admin/doctor/plan/add-new.blade.php
@@ -537,7 +537,7 @@ $.ajaxSetup({
                                     <label for="select_exercise_id${num}">${exercise_name} </label>
                                 </div>
                                 <div class="exercise-img">
-                                    <img src="${value.image_url ? value.image_url : '{{ asset('dist/img/placeholder.png') }}'}" class="" height="120px" width="150px">
+                                    <img src="${value.image_url ? value.image_url : '{{ asset('dist/img/placeholder.png') }}'}" class="" height="155px" width="225px">
                                 </div>
                             </div>
                         </div>`;
